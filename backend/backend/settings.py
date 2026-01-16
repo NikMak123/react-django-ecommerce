e517f9d79bfd24a40e6c35c275a51a66d7d82122
NikMak123 <nikhil.makwana9999@gmail.com>
2026-01-17 00:20:02 +0530

    Fix CSRF for Railway admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,14 +108,23 @@ else:
 # =========================
 # DRF + JWT (IMPORTANT)
 # =========================
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated',
+#     ),
+# }
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
 }
+
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=2000),
